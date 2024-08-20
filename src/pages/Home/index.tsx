6df28d0f46c91ced5dfcf0dbbaaa65bfde6e8978
MandooMandooMan <96804163+jwonlee95@users.noreply.github.com>
2024-08-20 20:58:35 +0900

    Add sections at home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,21 @@
-import { AppWrapper } from 'components';
-import React from 'react';
-
+import { AppWrapper } from "components";
+import React from "react";
 
 export const Home: React.FC<{}> = () => {
   return (
     <AppWrapper title="Home">
-        <div> Home </div>
+      <section className="first-section">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the first section.</p>
+      </section>
+      <section className="second-section">
+        <h2>Second Section</h2>
+        <p>This is some content in the second section.</p>
+      </section>
+      <section className="third-section">
+        <h2>Third Section</h2>
+        <p>This is some content in the third section.</p>
+      </section>
     </AppWrapper>
   );
 };
