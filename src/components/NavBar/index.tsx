4dd2c92a181253d53ff92e5e607e8130852d5f41
MandooMandooMan <96804163+jwonlee95@users.noreply.github.com>
2024-08-20 20:59:11 +0900

    Handle navbar position

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,11 +1,24 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Stack, Button, IconButton } from "@mui/material";
 import { IMAGES } from "assets/images";
 
 
 export const NavBar: React.FC<{}> = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
   const navigate = useNavigate();
+
+  const handleScroll = () => {
+    const scrollTop = window.scrollY;
+    setIsScrolled(scrollTop > 0);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const handleClickLogo = () => {
     navigate("/");
@@ -16,7 +29,7 @@ export const NavBar: React.FC<{}> = () => {
   };
 
   return (
-    <nav>
+    <nav className={`navbar ${isScrolled ? "scrolled" : ""}`}>
       <div className="logo">
         <img src={IMAGES.logo} alt="logo" onClick={handleClickLogo} />
       </div>
